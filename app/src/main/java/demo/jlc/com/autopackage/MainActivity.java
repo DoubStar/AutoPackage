f82bd839031666cc37581835d3bd4581953adefc
DoubStar <jlc_1981@qq.com>
2016-12-29 23:07:00 +0800

    Initial commit

---
+++ app/src/main/java/demo/jlc/com/autopackage/MainActivity.java
@@ -0,0 +1,13 @@
+package demo.jlc.com.autopackage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
